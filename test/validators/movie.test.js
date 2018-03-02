41eb64a607b39e3a402f90a91bc97ade04a0fa5e
jaonjohnkim <kim.jaon79@gmail.com>
2018-03-02 13:47:34 -0800

    nit fixes

--- test/validators/movie.test.js
+++ test/validators/movie.test.js
@@ -13,18 +13,14 @@ describe('movie validator', () => {
       const result = Joi.validate(payload, MovieValidator);
 
       expect(result.error.details[0].path[0]).to.eql('title');
-
       expect(result.error.details[0].type).to.eql('any.required');
     });
 
     it('is less than 255 characters', () => {
-      const payload = {
-        title: 'a'.repeat(260)
-      };
+      const payload = { title: 'a'.repeat(260) };
       const result = Joi.validate(payload, MovieValidator);
 
       expect(result.error.details[0].path[0]).to.eql('title');
-
       expect(result.error.details[0].type).to.eql('string.max');
     });
 
@@ -40,7 +36,6 @@ describe('movie validator', () => {
       const result = Joi.validate(payload, MovieValidator);
 
       expect(result.error.details[0].path[0]).to.eql('release_year');
-
       expect(result.error.details[0].type).to.eql('number.min');
 
     });
@@ -53,11 +48,9 @@ describe('movie validator', () => {
       const result = Joi.validate(payload, MovieValidator);
 
       expect(result.error.details[0].path[0]).to.eql('release_year');
-
       expect(result.error.details[0].type).to.eql('number.max');
     });
 
   });
 
 });
-
