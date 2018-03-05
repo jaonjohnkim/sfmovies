83dcb3cd3365209dc84f6f30944e824c9b6a54b4
jaonjohnkim <kim.jaon79@gmail.com>
2018-03-05 11:08:11 -0800

    removed redundancy in the unit test for the controller

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const Controller = require('../../../../lib/plugins/features/movies/controller');
-const Movie      = require('../../../../lib/models/movie');
 
 describe('movie controller', () => {
 
@@ -11,11 +10,6 @@ describe('movie controller', () => {
       const payload = { title: 'WALL-E' };
 
       return Controller.create(payload)
-      .then((movie) => {
-        expect(movie.get('title')).to.eql(payload.title);
-
-        return new Movie({ id: movie.id }).fetch();
-      })
       .then((movie) => {
         expect(movie.get('title')).to.eql(payload.title);
         expect(movie.get('name')).to.eql(payload.title);
