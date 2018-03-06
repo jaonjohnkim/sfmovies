d7d137e293ebd8bffad0fed935a69f0260f4597b
jaonjohnkim <kim.jaon79@gmail.com>
2018-03-06 09:47:04 -0800

    adds where clause to check for null

--- db/migrations/20180302174739_backfill_movies_name.js
+++ db/migrations/20180302174739_backfill_movies_name.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.up = (Knex, Promise) => {
-  return Knex.raw('UPDATE movies SET name = title');
+  return Knex.raw('UPDATE movies SET name = title WHERE name IS NULL');
 };
 
 exports.down = (Knex, Promise) => {
